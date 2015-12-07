e168d18f7bd5c0dad6a887b81df0a5a65517832e
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2015-12-07 22:38:34 +0100

    VErsion OK

---
+++ example.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"github.com/owulveryck/gorchestrator/structure"
+)
+
+var valid structure.Input
+var notValid structure.Input
+
+func init() {
+	valid = structure.Input{"Valid",
+		[]int64{0, 1, 0, 0, 1, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 1, 0,
+			0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 1, 0, 0, 0, 1, 0,
+			0, 0, 0, 0, 0, 0, 0, 0,
+			1, 1, 1, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 1, 0,
+		},
+		[]structure.Node{
+			{0, "a", "ansible", "myplaybook.yml", nil, nil},
+			{1, "b", "shell", "myscript.sh", nil,
+				map[string]string{
+					"output1": "",
+				},
+			},
+			{2, "c", "shell", "myscript2.sh",
+				[]string{
+					"-e", "get_attribute 1:output1",
+				}, nil},
+			{3, "d", "ansible", "myplaybook3.yml", nil, nil},
+			{4, "e", "ansible", "myplaybook4.yml", nil, nil},
+			{5, "f", "ansible", "myplaybook5.yml", nil, nil},
+			{6, "g", "ansible", "myplaybook6.yml", nil, nil},
+			{7, "h", "ansible", "myplaybook7.yml", nil, nil},
+		},
+	}
+
+	notValid = structure.Input{"NotValid",
+		[]int64{0, 1, 0, 0, 1, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 1, 0,
+			0, 0, 0, 0, 0, 0, 0, 0,
+			0, 1, 0, 0, 0, 1, 0,
+			0, 0, 0, 0, 0, 0, 0, 0,
+			1, 1, 1, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 1, 0,
+		},
+		[]structure.Node{
+			{0, "a", "ansible", "myplaybook.yml", nil, nil},
+			{1, "b", "shell", "myscript.sh", nil,
+				map[string]string{
+					"output1": "",
+				},
+			},
+			{2, "c", "shell", "myscript2.sh",
+				[]string{
+					"-e", "get_attribute 1:output1",
+				}, nil},
+			{3, "d", "ansible", "myplaybook3.yml", nil, nil},
+			{4, "e", "ansible", "myplaybook4.yml", nil, nil},
+			{5, "f", "ansible", "myplaybook5.yml", nil, nil},
+			{6, "g", "ansible", "myplaybook6.yml", nil, nil},
+			{7, "h", "ansible", "myplaybook7.yml", nil, nil},
+		},
+	}
+}
--- orchestrator.go
+++ orchestrator.go
@@ -3,24 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/owulveryck/gorchestrator/structure"
-	"math/rand"
 	"sync"
 	"time"
 )
-
-const (
-	ToRun       = 1
-	Running     = 2
-	Success     = 3
-	Failure     = 4
-	NotRunnable = 5
-)
-
-type Message struct {
-	id    int
-	state int
-	wait  chan structure.Matrix
-}
 
 type lock int
 
@@ -33,27 +18,18 @@ func (l *lock) Unlock() {
 
 func main() {
 	// Allocate a zeroed array of size 8×8
-	m := structure.Matrix{
-		0, 1, 0, 0, 1, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 1, 0,
-		0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 1, 0, 0, 0, 1, 0,
-		0, 0, 0, 0, 0, 0, 0, 0,
-		1, 1, 1, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 1, 0,
-	}
+	m := valid.Digraph
 
 	n := m.Dim()
-	cs := make([]<-chan Message, n)
+	cs := make([]<-chan structure.Message, n)
 	l := new(lock)
 	co := sync.NewCond(l)
 	for i := 0; i < n; i++ {
-		cs[i] = run(i, time.Duration(rand.Intn(1e3))*time.Millisecond)
+		cs[i] = valid.Nodes[i].Run()
 		node := <-cs[i]
 		go func() {
 			for {
-				node.wait <- m
+				node.Wait <- m
 				l.Lock()
 				defer l.Unlock()
 				co.Wait()
@@ -65,10 +41,10 @@ func main() {
 	for {
 		select {
 		case node := <-c:
-			if node.state >= Running {
-				fmt.Printf("%v has finished (%v)\n", node.id, node.state)
+			if node.State >= structure.Running {
+				fmt.Printf("%v has finished (%v)\n", node.ID, node.State)
 				for c := 0; c < n; c++ {
-					m.Set(node.id, c, int64(node.state))
+					m.Set(node.ID, c, int64(node.State))
 				}
 				co.Broadcast()
 			}
@@ -76,7 +52,7 @@ func main() {
 			fmt.Println("Timeout")
 			return
 		default:
-			if m.Sum() >= int64(Success*n*n) {
+			if m.Sum() >= int64(structure.Success*n*n) {
 				fmt.Println("All done!")
 				return
 			}
@@ -84,48 +60,8 @@ func main() {
 	}
 }
 
-func run(id int, duration time.Duration) <-chan Message {
-	c := make(chan Message)
-	waitForIt := make(chan structure.Matrix) // Shared between all messages.
-	go func() {
-		state := ToRun
-		for state <= ToRun {
-			c <- Message{id, state, waitForIt}
-			m := <-waitForIt
-			s := m.Dim()
-			state = Running
-			for i := 0; i < s; i++ {
-				if m.At(i, id) < Success && m.At(i, id) > 0 {
-					state = ToRun
-				} else if m.At(i, id) >= Failure {
-					state = NotRunnable
-					continue
-				}
-			}
-			if state == NotRunnable {
-				fmt.Printf("I am %v, and I cannot run\n", id)
-				c <- Message{id, state, waitForIt}
-			}
-			if state == Running {
-				fmt.Printf("I am %v, and I am running\n", id)
-				time.Sleep(duration)
-				rand.Seed(time.Now().Unix())
-				if rand.Intn(100) < 50 {
-					state = Success
-				} else {
-					state = Failure
-				}
-				// Now send the message that I'm done...
-				c <- Message{id, state, waitForIt}
-			}
-		}
-		close(c)
-	}()
-	return c
-}
-
-func fanIn(inputs ...<-chan Message) <-chan Message {
-	c := make(chan Message)
+func fanIn(inputs ...<-chan structure.Message) <-chan structure.Message {
+	c := make(chan structure.Message)
 	for i := range inputs {
 		input := inputs[i] // New instance of 'input' for each loop.
 		go func() {
