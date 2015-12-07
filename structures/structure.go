f0725b95538958b741578c68924b75560af2c735
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2015-12-07 15:11:18 +0000

    quote quote quote

--- structures/structure.go
+++ structures/structure.go
@@ -1,18 +1,18 @@
 package structure
 
 type Input struct {
-	Name    string `json:name,omitempty`
-	Digraph []int  `json:digraph`
-	Nodes   []Node `json:nodes`
+	Name    string `json:"name",omitempty`
+	Digraph []int  `json:"digraph"`
+	Nodes   []Node `json:"nodes"`
 }
 
 type Node struct {
-	ID       int               `json:id`
-	Name     string            `json:name,omitempty`
-	Engine   string            `json:engine:omitempty` // The execution engine (ie ansible, shell); aim to be like a shebang in a shell file
-	Artifact string            `json:artifact`
-	Args     []string          `json:args,omitempty`   // the arguments of the artifact, if needed
-	Outputs  map[string]string `json:output,omitempty` // the key is the name of the parameter, the value its value (always a string)
+	ID       int               `json:"id"`
+	Name     string            `json:"name",omitempty`
+	Engine   string            `json:"engine",omitempty` // The execution engine (ie ansible, shell); aim to be like a shebang in a shell file
+	Artifact string            `json:"artifact"`
+	Args     []string          `json:"args",omitempty`   // the arguments of the artifact, if needed
+	Outputs  map[string]string `json:"output",omitempty` // the key is the name of the parameter, the value its value (always a string)
 }
 
 type Error struct {
