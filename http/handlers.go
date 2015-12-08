dd9d906a71ed1e972220e2b1572fb0a82be1896a
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2015-12-08 14:22:17 +0000

    Initial release

---
+++ http/handlers.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/owulveryck/gorchestrator/orchestrator"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+/*
+Test with this curl command:
+
+*/
+func TaskCreate(w http.ResponseWriter, r *http.Request) {
+	var v orchestrator.Input
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+	if err != nil {
+		panic(err)
+	}
+	if err := r.Body.Close(); err != nil {
+		panic(err)
+	}
+	if err := json.Unmarshal(body, &v); err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusBadRequest) // unprocessable entity
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			panic(err)
+		}
+	}
+
+	v.Run(nil)
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusAccepted)
+}
---
+++ test.sh
@@ -0,0 +1,2 @@
+ curl -v -X POST -H 'Content-Type:application/json' -H 'Accept:application/json' -d @example.json -k http://localhost:8080/v1/tasks
+
