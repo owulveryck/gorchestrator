f3e889aaf9dc3f02dc37519bab65191b1d122dde
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2015-12-08 14:22:17 +0000

    Initial release

---
+++ http/pubsub.go
@@ -0,0 +1,6 @@
+package main
+
+// Publish a node request on the 0MQ bus
+func PublishNodeRequest(interface{}) interface{} {
+	return nil
+}
---
+++ http/router.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func NewRouter() *mux.Router {
+
+	router := mux.NewRouter().StrictSlash(true)
+	for _, route := range routes {
+		var handler http.Handler
+
+		handler = route.HandlerFunc
+		handler = Logger(handler, route.Name)
+
+		router.
+			Methods(route.Method).
+			Path(route.Pattern).
+			Name(route.Name).
+			Handler(handler)
+
+	}
+
+	router.Methods("GET").PathPrefix("/apidocs").Name("Apidocs").Handler(http.StripPrefix("/apidocs", http.FileServer(http.Dir("./dist"))))
+	return router
+}
