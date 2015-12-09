780bf159169befbdd263ad6cbdc794c1367f63d1
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2015-12-09 14:46:28 +0000

    Initial release

---
+++ clients/vizualize/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/awalterschulze/gographviz"
+	"github.com/owulveryck/gorchestrator/orchestrator"
+	"os"
+	"strconv"
+)
+
+func main() {
+	// Creates a new graph
+	g := gographviz.NewGraph()
+	// Now read the json input
+	var v orchestrator.Graph
+	dec := json.NewDecoder(os.Stdin)
+	if err := dec.Decode(&v); err != nil {
+		panic(err)
+
+	}
+
+	// Now for each node, create a node
+	g.SetName(v.Name)
+	g.SetDir(true)
+	m := make(map[int]string)
+	// Now add every node
+	for _, n := range v.Nodes {
+		g.AddNode("G", n.Name,
+			map[string]string{
+				"id":    fmt.Sprintf("\"%v\"", strconv.Itoa(n.ID)),
+				"label": fmt.Sprintf("\"%v|%v|%v\"", n.Name, n.Engine, n.Artifact),
+				"shape": "\"record\"",
+			})
+		m[n.ID] = n.Name
+	}
+	for r := 0; r < v.Digraph.Dim(); r++ {
+		for c := 0; c < v.Digraph.Dim(); c++ {
+			if v.Digraph.At(r, c) == 1 {
+				g.AddEdge(m[r], m[c], true, nil)
+			}
+		}
+	}
+	// Now add the edges
+	//g.AddEdge("Hello", "World", true, nil)
+	s := g.String()
+	fmt.Println(s)
+}
