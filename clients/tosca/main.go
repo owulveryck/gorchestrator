ca01de7eb19a03707ca59916ab28e36638c62738
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2015-12-10 16:03:26 +0100

    Do not take c and r, only r is needed as the matrix is a squared one

--- clients/tosca/main.go
+++ clients/tosca/main.go
@@ -15,11 +15,12 @@ func main() {
 		panic(err)
 	}
 	// Fill the digraph
-	r, c := t.AdjacencyMatrix.Dims()
-	v.Digraph = make([]int64, r*r+r)
-	for i := 0; i < r; i++ {
-		for j := 0; j < c; j++ {
-			v.Digraph[r*i+j] = int64(t.AdjacencyMatrix.At(i, j))
+	s, _ := t.AdjacencyMatrix.Dims()
+	v.Digraph = make([]int64, s*s+s)
+	v.Nodes = make([]orchestrator.Node, s)
+	for i := 0; i < s; i++ {
+		for j := 0; j < s; j++ {
+			v.Digraph[s*i+j] = int64(t.AdjacencyMatrix.At(i, j))
 		}
 	}
 	fmt.Println(v)
