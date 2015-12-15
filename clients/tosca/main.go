20860d46b58e68cc4ed5602ce9c1bd13e4fffce9
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2015-12-15 07:55:11 +0100

    Begining of the imports implementation

--- clients/tosca/main.go
+++ clients/tosca/main.go
@@ -30,82 +30,94 @@ import (
 )
 
 func main() {
-	var ts []toscalib.ToscaDefinition
+	var t toscalib.ToscaDefinition
+	var imports []toscalib.ToscaDefinition
+	imports = make([]toscalib.ToscaDefinition, 0)
 	var v orchestrator.Graph
 	args := os.Args[1:]
-	if len(args) == 0 {
-		ts = make([]toscalib.ToscaDefinition, 1)
-		err := ts[0].Parse(os.Stdin)
+	switch len(args) {
+	case 0:
+		err := t.Parse(os.Stdin)
 		if err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
-	} else {
-		ts = make([]toscalib.ToscaDefinition, len(args))
-		for t, f := range args {
-			// Read the file and parses the graph
-			re, err := os.Open(f)
-			if err != nil {
-				log.Println(err)
-				continue
-			}
-			ts[t].Parse(re)
+	case 1:
+		r, err := os.Open(args[0])
+		if err != nil {
+			log.Fatal(err)
 		}
+		err = t.Parse(r)
+		if err != nil {
+			log.Fatal(err)
+		}
+	default:
+		log.Fatal("Too many arguments")
 	}
-	for t, _ := range args {
-		// Fill the digraph
-		fmt.Println(ts[t].TopologyTemplate)
-		s, _ := ts[t].AdjacencyMatrix.Dims()
-		v.Digraph = make([]int64, s*s+s)
-		v.Nodes = make([]orchestrator.Node, s)
-		for i := 0; i < s; i++ {
-			v.Nodes[i].ID = i
-			n := ts[t].GetNodeTemplateFromId(i)
-			// Fill in a map with method as key and artifact as value
-			interfaces := make(map[string]string, 0)
-			for _, intf := range n.Interfaces {
-				for val, vv := range intf {
-					switch reflect.TypeOf(vv).Kind() {
-					case reflect.String:
-						interfaces[val] = reflect.ValueOf(vv).String()
-					case reflect.Map:
-						interfaces[val] = fmt.Sprintf("%v", reflect.ValueOf(vv).MapIndex(reflect.ValueOf("implementation")))
-					default:
-						interfaces[val] = "Found"
-					}
+	// Fill the digraph
+	// Deal with the imports
+	for i, im := range t.Imports {
+		log.Println("Importing ", im)
+		r, err := os.Open(im)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = t.Parse(r)
+		if err != nil {
+			log.Fatal(err)
+		}
+		imports[i] = t
+	}
+	s, _ := t.AdjacencyMatrix.Dims()
+	v.Digraph = make([]int64, s*s+s)
+	v.Nodes = make([]orchestrator.Node, s)
+	for i := 0; i < s; i++ {
+		v.Nodes[i].ID = i
+		n := t.GetNodeTemplateFromId(i)
+		// Fill in a map with method as key and artifact as value
+		interfaces := make(map[string]string, 0)
+		for _, intf := range n.Interfaces {
+			for val, vv := range intf {
+				switch reflect.TypeOf(vv).Kind() {
+				case reflect.String:
+					interfaces[val] = reflect.ValueOf(vv).String()
+				case reflect.Map:
+					interfaces[val] = fmt.Sprintf("%v", reflect.ValueOf(vv).MapIndex(reflect.ValueOf("implementation")))
+				default:
+					interfaces[val] = "Found"
 				}
-			} // FIXME
-			var op string
-			switch i {
-			case n.GetConfigureIndex():
-				op = "configure"
-			case n.GetStartIndex():
-				op = "start"
-			case n.GetStopIndex():
-				op = "stop"
-			case n.GetCreateIndex():
-				op = "create"
-			case n.GetDeleteIndex():
-				op = "delete"
-			case n.GetInitialIndex():
-				op = "initial"
-			case n.GetPostConfigureSourceIndex():
-				op = "postConfigureSource"
-			case n.GetPostConfigureTargetIndex():
-				op = "postConfigureTarget"
-			case n.GetPreConfigureSourceIndex():
-				op = "preConfigureSource"
-			case n.GetPreConfigureTargetIndex():
-				op = "preConfiguretarget"
 			}
-			v.Nodes[i].Artifact = interfaces[op]
-
-			v.Nodes[i].Name = fmt.Sprintf("%v:%v", n.Name, op)
-			for j := 0; j < s; j++ {
-				v.Digraph[s*i+j] = int64(ts[t].AdjacencyMatrix.At(i, j))
-			}
+		} // FIXME
+		var op string
+		switch i {
+		case n.GetConfigureIndex():
+			op = "configure"
+		case n.GetStartIndex():
+			op = "start"
+		case n.GetStopIndex():
+			op = "stop"
+		case n.GetCreateIndex():
+			op = "create"
+		case n.GetDeleteIndex():
+			op = "delete"
+		case n.GetInitialIndex():
+			op = "initial"
+		case n.GetPostConfigureSourceIndex():
+			op = "postConfigureSource"
+		case n.GetPostConfigureTargetIndex():
+			op = "postConfigureTarget"
+		case n.GetPreConfigureSourceIndex():
+			op = "preConfigureSource"
+		case n.GetPreConfigureTargetIndex():
+			op = "preConfiguretarget"
 		}
+		v.Nodes[i].Artifact = interfaces[op]
 
-		r, _ := json.MarshalIndent(v, "  ", "  ")
-		fmt.Print("%s\n", string(r))
+		v.Nodes[i].Name = fmt.Sprintf("%v:%v", n.Name, op)
+		for j := 0; j < s; j++ {
+			v.Digraph[s*i+j] = int64(t.AdjacencyMatrix.At(i, j))
+		}
 	}
+
+	r, _ := json.MarshalIndent(v, "  ", "  ")
+	fmt.Print("%s\n", string(r))
 }
