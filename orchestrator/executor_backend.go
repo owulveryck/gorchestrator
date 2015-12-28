384052bf0a90f617847e7aad6eb392037696c235
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2015-12-28 13:45:49 +0000

    Initial release

---
+++ orchestrator/executor_backend.go
@@ -0,0 +1,48 @@
+package orchestrator
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
+	"net/http"
+)
+
+// ExecutorBackend represent an executor
+type ExecutorBackend struct {
+	Url         string // eg: https://localhost:8585/v1
+	Certificate string //
+	Key         string
+	CACert      string
+	Ping        string // eg /ping
+	Client      *http.Client
+}
+
+func (e *ExecutorBackend) init() error {
+	var client *http.Client
+	// Load client cert
+	cert, err := tls.LoadX509KeyPair(e.Certificate, e.Key)
+	if err != nil {
+		return err
+	}
+
+	// Load CA cert
+	caCert, err := ioutil.ReadFile(e.CACert)
+	if err != nil {
+		return err
+
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	// Setup HTTPS client
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		RootCAs:      caCertPool,
+	}
+	tlsConfig.BuildNameToCertificate()
+	transport := &http.Transport{TLSClientConfig: tlsConfig}
+	client = &http.Client{Transport: transport}
+
+	e.Client = client
+	return nil
+}
