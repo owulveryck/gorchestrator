e9398328c907b26a3f07e1f07bea3d8da2c91335
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2015-12-29 11:06:04 +0000

    Do not move files anymore

--- security/util/generate.sh
+++ security/util/generate.sh
@@ -2,4 +2,3 @@ echo "Generating Orchestrator certificate"
 go run generate_cert.go -ca -host 127.0.0.1 -target orchestrator
 echo "Generating Executor certificate"
 go run generate_cert.go -ca -host 127.0.0.1 -target executor
-mv *.pem ../../tests/binaries/
