acfabaffe669637ca22f4aff3d4a15dab85ced8d
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2016-01-04 21:07:15 +0100

    Template is more and more responsive

--- clients/web/webhandler.go
+++ clients/web/webhandler.go
@@ -72,7 +72,7 @@ func displayMain(w http.ResponseWriter, r *http.Request) {
 	}
 
 	t := template.New("template.tmpl")
-	t, err = t.ParseFiles("tmpl/template.tmpl", "tmpl/viewgraph.tmpl")
+	t, err = t.ParseFiles("tmpl/template.tmpl", "tmpl/viewgraph.tmpl", "tmpl/navbar_right.tmpl")
 	if err != nil {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 		w.WriteHeader(http.StatusNotFound)
