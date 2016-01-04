07841a6baed6c2f3c11cb73592b30d1590e4b153
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2016-01-04 13:42:28 +0000

    New fied in the node entry: "target"

--- orchestrator/node.go
+++ orchestrator/node.go
@@ -35,7 +35,8 @@ import (
 type Node struct {
 	ID       int               `json:"id"`
 	State    int               `json:"state,omitempty"`
-	Name     string            `json:"name,omitempty"`
+	Name     string            `json:"name,omitempty"`   // The targeted host
+	Target   string            `json:"target,omitempty"` // The execution engine (ie ansible, shell); aim to be like a shebang in a shell file
 	Engine   string            `json:"engine,omitempty"` // The execution engine (ie ansible, shell); aim to be like a shebang in a shell file
 	Artifact string            `json:"artifact"`
 	Args     []string          `json:"args,omitempty"`   // the arguments of the artifact, if needed
