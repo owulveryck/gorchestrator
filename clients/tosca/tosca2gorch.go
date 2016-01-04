42a04c3825d97558d094c4b3f33ee48ecfd4417c
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2016-01-04 09:31:05 +0000

    [Fix] Compatibility with the new toscalib

--- clients/tosca/tosca2gorch.go
+++ clients/tosca/tosca2gorch.go
@@ -23,154 +23,32 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/owulveryck/gorchestrator/orchestrator"
-	//"github.com/owulveryck/gorchestrator/structure"
-	"github.com/owulveryck/gorchestrator/Godeps/_workspace/src/github.com/owulveryck/toscalib"
+	"github.com/owulveryck/gorchestrator/structure"
+	"github.com/owulveryck/toscalib"
+	"github.com/owulveryck/toscalib/toscaexec"
 	"log"
 	"os"
-	//"reflect"
 )
 
-func togorch(t toscalib.ToscaDefinition) orchestrator.Graph {
-	var v orchestrator.Graph
-	var exclusion []int
-	exclusion = make([]int, 0)
-	s, _ := t.AdjacencyMatrix.Dims()
-	v.Digraph = make([]int64, s*s)
-	v.Nodes = make([]orchestrator.Node, s)
-	for i := 0; i < s; i++ {
-		v.Nodes[i].ID = i
-		n := t.GetNodeTemplateFromId(i)
-		// Fill in a map with method as key and artifact as value
-		interfaces := make(map[string]string, 0)
-		args := make(map[string][]string, 0)
-		for _, intf := range n.Interfaces {
-			for method, interfaceDefinition := range intf {
-				interfaces[method] = interfaceDefinition.Implementation
-				// Fill the args
-				for key, value := range interfaceDefinition.Inputs {
-					for v, vv := range value {
-						switch v {
-						case "value":
-							args[method] = append(args[method], fmt.Sprintf("%v=%v", key, vv))
-						case "get_input":
-							args[method] = append(args[method], fmt.Sprintf("%v=%v", key, t.TopologyTemplate.Inputs[vv].Default))
-						default:
-							args[method] = append(args[method], fmt.Sprintf("%v=%v(%v)", key, v, vv))
-						}
-					}
-				}
-			}
-		}
-		// FIXME
-		var op string
-		switch i {
-		case n.GetConfigureIndex():
-			op = "configure"
-		case n.GetStartIndex():
-			op = "start"
-		case n.GetStopIndex():
-			op = "stop"
-		case n.GetCreateIndex():
-			op = "create"
-		case n.GetDeleteIndex():
-			op = "delete"
-		case n.GetInitialIndex():
-			op = "initial"
-		case n.GetPostConfigureSourceIndex():
-			op = "postConfigureSource"
-		case n.GetPostConfigureTargetIndex():
-			op = "postConfigureTarget"
-		case n.GetPreConfigureSourceIndex():
-			op = "preConfigureSource"
-		case n.GetPreConfigureTargetIndex():
-			op = "preConfiguretarget"
-		}
-		if _, ok := interfaces[op]; !ok {
-			// Look for the implementation in the type
-			for _, intf := range t.NodeTypes[n.Type].Interfaces {
-				for method, interfaceDefinition := range intf {
-					interfaces[method] = interfaceDefinition.Implementation
-					// key holds the input name
-					for key, _ := range interfaceDefinition.Inputs {
-						if prop, ok := n.Properties[key]; !ok {
-							log.Println("No Value found for input", key)
-						} else {
-							for v, vv := range prop {
-								switch v {
-								case "value":
-									args[method] = append(args[method], fmt.Sprintf("%v=%v", key, vv))
-								case "get_input":
-									args[method] = append(args[method], fmt.Sprintf("%v=%v", key, t.TopologyTemplate.Inputs[vv].Default))
-								default:
-									args[method] = append(args[method], fmt.Sprintf("%v=%v(%v)", key, v, vv))
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-		v.Nodes[i].Artifact = interfaces[op]
-		v.Nodes[i].Args = args[op]
-		//v.Nodes[i].Args = args[op]
-
-		if op != "" {
-			v.Nodes[i].Name = fmt.Sprintf("%v:%v", n.Name, op)
-			if interfaces[op] == "nil" || interfaces[op] == "" {
-				exclusion = append(exclusion, i)
-			}
-		} else {
-			v.Nodes[i].Name = fmt.Sprintf("%v", n.Name)
-		}
-		log.Printf("[%v] %v", v.Nodes[i].Name, interfaces[op])
-		for j := 0; j < s; j++ {
-			v.Digraph[s*i+j] = int64(t.AdjacencyMatrix.At(i, j))
-		}
+func togorch(t toscalib.ServiceTemplateDefinition) orchestrator.Graph {
+	e := toscaexec.GeneratePlaybook(t)
+	var g orchestrator.Graph
+	g.Digraph = structure.Matrix(e.AdjacencyMatrix)
+	g.Name = t.Description
+	for i, n := range e.Index {
+		var node orchestrator.Node
+		node.ID = i
+		node.Name = fmt.Sprintf("%v:%v", n.NodeTemplate.Name, n.OperationName)
+		node.Engine = "shell"
+		node.Artifact = n.NodeTemplate.Interfaces[n.InterfaceName].Operations[n.OperationName].Implementation
+		//node.Args = n.NodeTemplate.Interfaces[n.InterfaceName].Inputs
+		g.Nodes = append(g.Nodes, node)
 	}
-	// now shrink the matrix... (remove the exclusion)
-	/*
-		log.Println("Exclusions", exclusion)
-		for _, i := range exclusion {
-
-			l := v.Digraph.Dim()
-			//Remove the i-th line
-			//= copy all the values if pos-i%l!=0
-			target := make(structure.Matrix, 0)
-			target2 := make(structure.Matrix, 0)
-			var value int64
-			for pos := 0; pos < l; pos++ {
-				value = v.Digraph[pos]
-				if (pos-i)%l != 0 {
-					log.Printf("Pos: %v, value: %v, i:%v, l:%v", pos, value, i, l)
-					target = append(target, value)
-				}
-			}
-			// Remove the i-th column
-			log.Println("TARGET", target)
-			for pos, value := range target {
-				if pos < i*(l-1) && pos > (i+1)*(l-1) {
-					target2 = append(target2, value)
-				}
-			}
-
-			// Now copy the new structure
-			v.Digraph = target2
-			// Now remove the node i
-
-			// And finally renum all the node > i to i-1
-		}
-	*/
-	return v
-}
-
-func shrink() {
-
+	return g
 }
 
 func main() {
-	var t toscalib.ToscaDefinition
-	var imports []toscalib.ToscaDefinition
-	imports = make([]toscalib.ToscaDefinition, 0)
+	var t toscalib.ServiceTemplateDefinition
 	var v orchestrator.Graph
 	args := os.Args[1:]
 	switch len(args) {
@@ -191,34 +69,8 @@ func main() {
 	default:
 		log.Fatal("Too many arguments")
 	}
-	// Fill the digraph
-	// Deal with the imports
-	for _, im := range t.Imports {
-		var tt toscalib.ToscaDefinition
-		log.Println("Importing ", im)
-		r, err := os.Open(im)
-		if err != nil {
-			log.Fatal(err)
-		}
-		err = tt.Parse(r)
-		if err != nil {
-			log.Fatal(err)
-		}
-		imports = append(imports, tt)
-	}
-
-	// Now reconstruct the global definition (only the types by now)
-	for _, i := range imports {
-		for key, m := range i.NodeTypes {
-			if _, ok := t.NodeTypes[key]; !ok {
-				t.NodeTypes[key] = m
-			}
-		}
-	}
 
 	v = togorch(t)
-	// Convert it to gorch
-	//r, _ := json.Marshal(v)
 	r, _ := json.MarshalIndent(v, "  ", "  ")
 	fmt.Printf("%s\n", string(r))
 }
