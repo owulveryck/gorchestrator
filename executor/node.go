bf54efd279c10fb9a98c034191c11592a4c52696
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2016-01-05 22:26:27 +0100

    New SSH plugin to be tested

--- executor/node.go
+++ executor/node.go
@@ -10,39 +10,53 @@ import (
 
 type node orchestrator.Node
 
+// shell runs the node via the shell plugin on host localhost
+func (n *node) shell() error {
+	d := exec.Command(n.Artifact, n.Args...)
+	// Set the stdin stdout and stderr of the dot subprocess
+	stdinOfDotProcess, err := d.StdinPipe()
+	if err != nil {
+		n.State = orchestrator.Failure
+		return err
+
+	}
+	defer stdinOfDotProcess.Close() // the doc says subProcess.Wait will close it, but I'm not sure, so I kept this line
+	readCloser, err := d.StdoutPipe()
+	if err != nil {
+		n.State = orchestrator.Failure
+		return err
+
+	}
+	d.Stderr = os.Stderr
+
+	// Actually run the dot subprocess
+	if err = d.Run(); err != nil { //Use start, not run
+		n.State = orchestrator.Failure
+		log.Println("An error occured: ", err) //replace with logger, or anything you want
+		return err
+	}
+	//fmt.Fprintf(stdinOfDotProcess, s)
+	stdinOfDotProcess.Close()
+
+	// Read from stdout and store it in the correct structure
+	var buf bytes.Buffer
+	buf.ReadFrom(readCloser)
+	n.State = orchestrator.Success
+
+	return nil
+}
+
 // Run actually launch the engine to run the node
 func (n *node) Run() {
 	log.Printf("Running %v with engine %v, artifact %v and args %v", n.Name, n.Engine, n.Artifact, n.Args)
 	if n.Artifact != "" {
-		d := exec.Command(n.Artifact, n.Args...)
-		// Set the stdin stdout and stderr of the dot subprocess
-		stdinOfDotProcess, err := d.StdinPipe()
-		if err != nil {
+		switch n.Engine {
+		case "shell":
+			_ = n.shell()
+		case "ssh":
+			_ = n.ssh()
+		case "default":
 			n.State = orchestrator.Failure
-			return
-
 		}
-		defer stdinOfDotProcess.Close() // the doc says subProcess.Wait will close it, but I'm not sure, so I kept this line
-		readCloser, err := d.StdoutPipe()
-		if err != nil {
-			n.State = orchestrator.Failure
-			return
-
-		}
-		d.Stderr = os.Stderr
-
-		// Actually run the dot subprocess
-		if err = d.Run(); err != nil { //Use start, not run
-			n.State = orchestrator.Failure
-			log.Println("An error occured: ", err) //replace with logger, or anything you want
-			return
-		}
-		//fmt.Fprintf(stdinOfDotProcess, s)
-		stdinOfDotProcess.Close()
-
-		// Read from stdout and store it in the correct structure
-		var buf bytes.Buffer
-		buf.ReadFrom(readCloser)
-		n.State = orchestrator.Success
 	}
 }
---
+++ executor/ssh_plugin.go
@@ -0,0 +1,217 @@
+package executor
+
+import (
+	"fmt"
+	"github.com/owulveryck/gorchestrator/orchestrator"
+	"gopkg.in/yaml.v2"
+	"io"
+	"io/ioutil"
+	"net"
+	"os"
+	"strings"
+
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/agent"
+)
+
+type sshConfig struct {
+	Host           string `yaml:"host"`
+	Port           int    `yaml:"port"`
+	PublicKeyFile  string `yaml:"public_key"`
+	PrivateKeyFile string `yaml:"private_key"`
+	User           string `yaml:"remote_user"`
+}
+
+type SSHCommand struct {
+	Path   string
+	Env    []string
+	Stdin  io.Reader
+	Stdout io.Writer
+	Stderr io.Writer
+}
+
+type SSHClient struct {
+	Config *ssh.ClientConfig
+	Host   string
+	Port   int
+}
+
+func (client *SSHClient) RunCommand(cmd *SSHCommand) error {
+	var (
+		session *ssh.Session
+		err     error
+	)
+
+	if session, err = client.newSession(); err != nil {
+		return err
+
+	}
+	defer session.Close()
+
+	if err = client.prepareCommand(session, cmd); err != nil {
+		return err
+
+	}
+
+	err = session.Run(cmd.Path)
+	return err
+
+}
+
+func (client *SSHClient) prepareCommand(session *ssh.Session, cmd *SSHCommand) error {
+	for _, env := range cmd.Env {
+		variable := strings.Split(env, "=")
+		if len(variable) != 2 {
+			continue
+
+		}
+
+		if err := session.Setenv(variable[0], variable[1]); err != nil {
+			return err
+
+		}
+
+	}
+
+	if cmd.Stdin != nil {
+		stdin, err := session.StdinPipe()
+		if err != nil {
+			return fmt.Errorf("Unable to setup stdin for session: %v", err)
+
+		}
+		go io.Copy(stdin, cmd.Stdin)
+
+	}
+
+	if cmd.Stdout != nil {
+		stdout, err := session.StdoutPipe()
+		if err != nil {
+			return fmt.Errorf("Unable to setup stdout for session: %v", err)
+
+		}
+		go io.Copy(cmd.Stdout, stdout)
+
+	}
+
+	if cmd.Stderr != nil {
+		stderr, err := session.StderrPipe()
+		if err != nil {
+			return fmt.Errorf("Unable to setup stderr for session: %v", err)
+
+		}
+		go io.Copy(cmd.Stderr, stderr)
+
+	}
+
+	return nil
+
+}
+
+func (client *SSHClient) newSession() (*ssh.Session, error) {
+	connection, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", client.Host, client.Port), client.Config)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to dial: %s", err)
+
+	}
+
+	session, err := connection.NewSession()
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create session: %s", err)
+
+	}
+
+	modes := ssh.TerminalModes{
+		// ssh.ECHO:          0,     // disable echoing
+		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+
+	}
+
+	if err := session.RequestPty("xterm", 80, 40, modes); err != nil {
+		session.Close()
+		return nil, fmt.Errorf("request for pseudo terminal failed: %s", err)
+
+	}
+
+	return session, nil
+
+}
+
+func PublicKeyFile(file string) ssh.AuthMethod {
+	buffer, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil
+
+	}
+
+	key, err := ssh.ParsePrivateKey(buffer)
+	if err != nil {
+		return nil
+
+	}
+	return ssh.PublicKeys(key)
+
+}
+
+func SSHAgent() ssh.AuthMethod {
+	if sshAgent, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK")); err == nil {
+		return ssh.PublicKeysCallback(agent.NewClient(sshAgent).Signers)
+
+	}
+	return nil
+
+}
+
+// ssh runs the node via the ssh plugin on host "target"
+func (n *node) ssh() error {
+	var conf map[string]sshConfig
+	r, err := os.Open("sshConfig.yaml")
+	if err != nil {
+		return err
+	}
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		return err
+	}
+	err = yaml.Unmarshal(data, &conf)
+	if err != nil {
+		return err
+	}
+	r.Close()
+
+	if _, ok := conf[n.Target]; !ok {
+		return fmt.Errorf("Cannot find entry for host %v in the ssh config file", n.Target)
+	}
+	// ssh.Password("your_password")
+	sshConfig := &ssh.ClientConfig{
+		User: conf[n.Target].User,
+		Auth: []ssh.AuthMethod{
+			PublicKeyFile(conf[n.Target].PublicKeyFile),
+			SSHAgent(),
+		},
+	}
+
+	client := &SSHClient{
+		Config: sshConfig,
+		Host:   conf[n.Target].Host,
+		Port:   conf[n.Target].Port,
+	}
+
+	cmd := &SSHCommand{
+		Path:   fmt.Sprintf("%v %v", n.Artifact, n.Args[0:]),
+		Env:    []string{"LC_DIR=/"},
+		Stdin:  os.Stdin,
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+	}
+
+	fmt.Printf("Running command: %s\n", cmd.Path)
+	n.State = orchestrator.Running
+	if err := client.RunCommand(cmd); err != nil {
+		n.State = orchestrator.Failure
+		fmt.Fprintf(os.Stderr, "command run error: %s\n", err)
+		return err
+	}
+	n.State = orchestrator.Success
+	return nil
+}
